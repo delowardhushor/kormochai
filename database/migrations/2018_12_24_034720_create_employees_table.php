66480eb4e40a68aa35e45cff785cdf2ff64c9e44
Unknown <delowarhossaintb@yahoo.com>
2018-12-24 10:12:31 +0600

    about to push

---
+++ database/migrations/2018_12_24_034720_create_employees_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('phone');
+            $table->string('password');
+            $table->string('name')->nullable();
+            $table->string('age')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('education')->nullable();
+            $table->string('address')->nullable();
+            $table->string('area')->nullable();
+            $table->string('post')->nullable();
+            $table->string('thana')->nullable();
+            $table->string('district')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
